b80dccdabe5902bde18c1441698901f4b5ce98bd
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2026-07-14 22:10:33 +0300

    feat(header): add function to set --header-container-height

--- src/scripts/layout/header.js
+++ src/scripts/layout/header.js
@@ -1,3 +1,5 @@
+import { mqlDesktop, mqlMobile } from "../utils/responsive.js";
+
 const header = document.querySelector(".header");
 
 const headerPaddingChanger = {
@@ -20,3 +22,20 @@ const headerPaddingChanger = {
 
 headerPaddingChanger.handleEvent();
 window.addEventListener("scroll", headerPaddingChanger);
+
+const mqls = [mqlDesktop, mqlMobile];
+
+function getHeaderContainerHeight() {
+    const containerHeight = header.children[0].scrollHeight;
+    const htmlFontSize = parseFloat(
+        getComputedStyle(document.documentElement).fontSize
+    );
+
+    header.style.setProperty(
+        "--header-container-height",
+        `${containerHeight / htmlFontSize}rem`
+    );
+}
+
+getHeaderContainerHeight();
+mqls.forEach((mql) => mql.addEventListener("change", getHeaderContainerHeight));
