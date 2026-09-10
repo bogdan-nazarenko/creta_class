b1b91a1e1f1d190f02953ae9fc40b36578bcb168
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2026-09-10 18:02:27 +0300

    Updates

---
+++ scripts/sections/faq.js
@@ -0,0 +1,1 @@
+import{accordionImpl}from"../templates/accordion.js";const faq=document.querySelector(".faq"),accordion=faq.querySelector(".accordion");accordionImpl.init(accordion);
---
+++ scripts/utils/responsive.js
@@ -0,0 +1,1 @@
+export const desktopMql=window.matchMedia("(width >= 1024px)");export const mobileMql=window.matchMedia("(width < 768px)");
